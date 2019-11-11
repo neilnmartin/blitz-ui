c6877891c2cc5679e9cf02451ce8f6341467c79d
Neil Martin <nnmartin94@gmail.com>
2019-11-10 23:46:57 -0800

    added sign up reducer to currentUserReducer

--- src/redux/reducers/currentUserReducer.js
+++ src/redux/reducers/currentUserReducer.js
@@ -1,33 +1,74 @@
-import {LOG_IN_REQUEST, LOG_IN_SUCCESS} from '../actions/actions'
+import {
+  LOG_IN_REQUEST,
+  LOG_IN_SUCCESS,
+  SIGN_UP_REQUEST,
+  SIGN_UP_SUCCESS
+} from "../actions/actions";
 
-function currentUserReducer(
+const currentUserReducer = (
   state = {
     loading: false,
     currentUser: {}
-  }, action) {
-    switch(action.type) {
-      case LOG_IN_REQUEST:
-        return Object.assign({}, state, {
-          loading: true
-        })
-      case LOG_IN_SUCCESS:
-        const { id, email, username, firstName, lastName } = action.payload
-        return Object.assign({}, state, {
-          loading: false,
-          currentUser: {
-            id,
-            email,
-            username,
-            firstName,
-            lastName
-          }
-        })
-      default:
-        return {
-          loading: false,
-          currentUser: {}
+  },
+  action
+) => {
+  switch (action.type) {
+    case LOG_IN_REQUEST:
+      return Object.assign({}, state, {
+        loading: true
+      });
+    case LOG_IN_SUCCESS:
+      const { id, email, username, firstName, lastName } = action.loginUserData;
+      return Object.assign({}, state, {
+        loading: false,
+        currentUser: {
+          id,
+          email,
+          username,
+          firstName,
+          lastName
         }
-    }
+      });
+    case SIGN_UP_REQUEST:
+      return Object.assign({}, state, {
+        loading: true
+      });
+    case SIGN_UP_SUCCESS:
+      const {
+        id,
+        email,
+        username,
+        firstName,
+        lastName
+      } = action.signupUserData;
+      return Object.assign({}, state, {
+        loading: false,
+        currentUser: {
+          id,
+          email,
+          username,
+          firstName,
+          lastName
+        }
+      });
+    default:
+      return {
+        loading: false,
+        currentUser: {}
+      };
+  }
 }
 
 export default currentUserReducer;
+
+/* let storeStateSample = {
+  loading: false,
+  currentUser: {
+    id,
+    email,
+    username,
+    firstName,
+    lastName,
+    token
+  },
+} */
