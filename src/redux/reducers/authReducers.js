21c60602485a76df42ca43fdb47d401ad3b685ac
Neil Martin <nnmartin94@gmail.com>
2019-11-12 16:47:19 -0800

    put log in and sign up reducers in authReducers file

---
+++ src/redux/reducers/authReducers.js
@@ -0,0 +1,89 @@
+import {
+  LOG_IN_REQUEST,
+  LOG_IN_SUCCESS,
+  SIGN_UP_REQUEST,
+  SIGN_UP_SUCCESS,
+  SIGN_UP_FAILURE,
+  LOG_OUT
+} from "../actions/actions";
+
+export const loginReducer = (
+  state = {
+    loading: false,
+    currentUser: {}
+  },
+  action
+) => {
+  switch (action.type) {
+    case LOG_IN_REQUEST:
+      return Object.assign({}, state, {
+        loading: true
+      });
+    case LOG_IN_SUCCESS:
+      let { id, email, username, firstName, lastName } = action.loginUserData;
+      return Object.assign({}, state, {
+        loading: false,
+        currentUser: {
+          id,
+          email,
+          username,
+          firstName,
+          lastName
+        }
+      });
+    case LOG_OUT: 
+      return Object.assign({}, state, {
+        loading: false,
+        currentUser: {}
+      })
+    default:
+      return {
+        loading: false,
+        currentUser: {}
+      };
+  }
+};
+
+export const signupReducer = (
+  state = {
+    loading: false,
+    currentUser: {}
+  },
+  action
+) => {
+  switch (action.type) {
+    case SIGN_UP_REQUEST:
+      return Object.assign({}, state, {
+        loading: true
+      });
+    case SIGN_UP_SUCCESS:
+      let { id, email, username, firstName, lastName } = action.signupUserData;
+      return Object.assign({}, state, {
+        loading: false,
+        currentUser: {
+          id,
+          email,
+          username,
+          firstName,
+          lastName
+        }
+      });
+    default:
+      return {
+        loading: false,
+        currentUser: {}
+      };
+  }
+};
+
+/* let storeStateSample = {
+  loading: false,
+  currentUser: {
+    id,
+    email,
+    username,
+    firstName,
+    lastName,
+    token
+  },
+} */
